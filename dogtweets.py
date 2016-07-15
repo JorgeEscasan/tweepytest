55583854fe2df75dea56156114f91065f18b535d
Jorge Escasan <jorgeaescasanr@gmail.com>
2016-07-15 03:00:22 -0700

    Initial commit

---
+++ dogtweets.py
@@ -0,0 +1,24 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+ckey = '9aj4GEuESp7ZQUROeoSQFXfrH'
+csecret = 'a0YWjZ0oHrz3NL2oY06R7HUnH0GeFXwEDurZq5EM352Qe3YOYS'
+atoken = '2249860142-9HZBr6dvOlSq7tAV237DG949ShAdkzggQ2laUTo'
+asecret = 'yntB2C592p5pW9tfaRtkQbKR9frdVM0t7xzQhhXJjxZyG'
+dog = 0
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        dog = len(data)
+        print len(data)
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["dog"])
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+#import dogtweets
 
 app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return 'Test'
+    return "test"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=4320)
